0a2a0da2507f6ac75d0c2f091cd266d362c50074
Shanmugapriya Boopathi <sboopathi@spplus.com>
2025-04-03 14:58:51 +0530

    recommondations API

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import router from "./routes/index.routes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", router);
 
