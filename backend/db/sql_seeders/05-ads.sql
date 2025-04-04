64311d82fdadaca0e106d580dfc488f362d56462
Shanmugapriya Boopathi <sboopathi@spplus.com>
2025-04-04 14:38:37 +0530

    seeder file chage

--- backend/db/sql_seeders/05-ads.sql
+++ backend/db/sql_seeders/05-ads.sql
@@ -1,14 +1,16 @@
 -- Ads seeder
 INSERT INTO ads (advertiserId, adUrl, imageUrl, locationIds, costToClick, categoryId)
 VALUES 
-(1, 'https://online.kfc.co.in/', 'https://cdn.i.haymarketmedia.asia/?n=campaign-asia%2Fcontent%2FKFC+OOH+1+HotSpicyCOB+land.jpg&c=0', '["1"]', 0.50, 1),
-(2, 'https://www.swiggy.com/city/chennai/order-online', 'https://etimg.etb2bimg.com/photo/110814623.cms', '["2","3"]', 0.75, 1),
-(3, 'https://wrogn.com/', 'https://wrogn.com/cdn/shop/files/BUY-2-T-SHIRTS-RS-999-mobile_8a327d0d-0657-4696-b452-ac017a40cc3d.jpg?v=1732292117', '["2"]', 0.80, 2),
-(4, 'https://puma.com/sale', 'https://puma.com/images/sports.jpg', '["3"]', 1.00, 2),
-(5, 'https://www.myherbalife.com/', 'https://www.nutritionleaders.co.uk/cdn/shop/products/IMG_0085_1200x1200.jpg?v=1617878796', '["3"]', 0.60, 3),
-(6, 'https://discoverpilgrim.com/', 'https://discoverpilgrim.com/cdn/shop/files/10_VitaminCFaceSerum_30ml_1080x1080.jpg?v=1742819714&width=1080', '["1","2","3"]', 0.90, 3),
-(7, 'https://gobumpr.com/', 'https://storage.googleapis.com/bkt-gobumper-prod-web-app-static/offers-imgs/lp-CCD3.jpg', '["1","2"]', 0.55, 7),
-(8, 'https://www.yokohama-india.com/', 'https://www.y-yokohama.com/global/product/tire/images/top/slide04_03.jpg', '["1","2","3"]', 0.85, 7), 
-(9, 'https://in.bookmyshow.com/offers', 'https://imagesvs.oneindia.com/img/2017/11/coupon-article14-16-1510818218.jpg', '["2","3"]', 0.95, 9),
-(10, 'https://sunburn.in/', 'https://gighub.club/mediafiles/cache/0b/9a/0b9ae23ffc293f0f6d46d5d1b83fd42b.webp', '["1","3"]', 0.70, 9),
-(11, 'https://3mcarcare.3mindia.co.in/', 'https://cdn4.singleinterface.com/files/enterprise/coverphoto/284051/Banner-4-4-2-27-06-24-06-38-21.jpg', '["2"]', 0.80, 10);
+(1, 'https://online.kfc.co.in/', 'https://cdn.i.haymarketmedia.asia/?n=campaign-asia%2Fcontent%2FKFC+OOH+1+HotSpicyCOB+land.jpg&c=0', '[1]', 0.50, 1),
+(2, 'https://www.swiggy.com/city/chennai/order-online', 'https://etimg.etb2bimg.com/photo/110814623.cms', '[2,3]', 0.75, 2),
+(5, 'https://www.myherbalife.com/', 'https://www.nutritionleaders.co.uk/cdn/shop/products/IMG_0085_1200x1200.jpg?v=1617878796', '[1,2,3]', 0.80, 3),
+(7, 'https://gobumpr.com/', 'https://storage.googleapis.com/bkt-gobumper-prod-web-app-static/offers-imgs/lp-CCD3.jpg', '[3]', 1.00, 5),
+(8, 'https://www.yokohama-india.com/', 'https://www.y-yokohama.com/global/product/tire/images/top/slide04_03.jpg', '[3]', 0.60, 1),
+(9, 'https://in.bookmyshow.com/offers', 'https://imagesvs.oneindia.com/img/2017/11/coupon-article14-16-1510818218.jpg', '[1,2,3]', 0.90, 4),
+(3, 'https://wrogn.com/', 'https://wrogn.com/cdn/shop/files/BUY-2-T-SHIRTS-RS-999-mobile_8a327d0d-0657-4696-b452-ac017a40cc3d.jpg?v=1732292117', '[1,2]', 0.55, 1),
+(4, 'https://puma.com/sale', 'https://puma.com/images/sports.jpg', '[1,2,3]', 0.85, 2), 
+(6, 'https://discoverpilgrim.com/', 'https://discoverpilgrim.com/cdn/shop/files/10_VitaminCFaceSerum_30ml_1080x1080.jpg?v=1742819714&width=1080', '[2,3]', 0.95, 3),
+(10, 'https://sunburn.in/', 'https://gighub.club/mediafiles/cache/0b/9a/0b9ae23ffc293f0f6d46d5d1b83fd42b.webp', '[1,3]', 0.70, 4),
+(11, 'https://3mcarcare.3mindia.co.in/', 'https://cdn4.singleinterface.com/files/enterprise/coverphoto/284051/Banner-4-4-2-27-06-24-06-38-21.jpg', '[2]', 0.80, 5);
+
+
